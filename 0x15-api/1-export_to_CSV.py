9e342bb18f0d8513f83b4acee2bf3dcd807d112a
loda26 <mokhaled26498@gmail.com>
2024-03-26 01:13:53 +0200

    task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Export api to csv"""
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
+
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for tmp in tasks:
+            completed = tmp.get('completed')
+            title_task = tmp.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
